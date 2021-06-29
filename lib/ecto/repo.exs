1cf0ea414bf1ad64fd5d6794a72e21104af9bb9a
diabolical-datum <86643743+diabolical-datum@users.noreply.github.com>
2021-06-29 09:22:54 -0700

    add Ecto.Repo.Preloader.maybe_pmap/3 note

--- lib/ecto/repo.exs
+++ lib/ecto/repo.exs
@@ -1,6 +1,10 @@
 defmodule MyRepo do
   # use Ecto.Repo, otp_app: :my_app, adapter: Ecto.Adapters.Postgres
 
+  @doc """
+  Compare to Ecto.Repo.Preloader.maybe_pmap/3
+  https://github.com/elixir-ecto/ecto/blob/master/lib/ecto/repo/preloader.ex
+  """
   def pmap(queryables_kw, opts \\ []) when is_list(queryables_kw) and is_list(opts) do
     if match?([_, _ | _], queryables_kw) and not checked_out?() do
       # We pass caller: self() so the ownership pool knows where to fetch the connection from and
